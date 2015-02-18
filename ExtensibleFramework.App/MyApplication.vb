5b8c293b4ba94ab80bb5aa50794e8c8bde62b98c
Franz Alex Gaisie-Essilfie <falexge@gmail.com>
2015-02-18 10:11:06 +0000

    Resolves plugins' referenced assemblies.
    Added application settings, as in Plugins

--- ExtensibleFramework.App/MyApplication.vb
+++ ExtensibleFramework.App/MyApplication.vb
@@ -3,6 +3,7 @@
     Partial Friend Class MyApplication
         Private _exeFile As String = Nothing
         Dim _appDataDir As String
+        Dim _settings As Core.Settings
 
         ''' <summary>Gets the application data directory.</summary>
         ''' <value>The application data directory.</value>
@@ -32,9 +33,21 @@
             End Get
         End Property
 
+        ''' <summary>Gets the application's settings.</summary>
+        Public ReadOnly Property Settings As ExtensibleFramework.Core.Settings
+            Get
+                Return _settings
+            End Get
+        End Property
+
         Private Sub MyApplication_Startup(sender As Object, e As ApplicationServices.StartupEventArgs) Handles Me.Startup
-            ' set the AppData path
+            ' set the AppData and EXE path
+            _exeFile = System.Reflection.Assembly.GetExecutingAssembly().Location
             _appDataDir = GetAppDataPath()
+            _settings = New Core.Settings(System.IO.Path.Combine(_appDataDir, "settings.json"))
+
+            ' allows loading of assemblies not in the current directory
+            AddHandler AppDomain.CurrentDomain.AssemblyResolve, AddressOf ResolveAssembly
         End Sub
 
         ''' <summary>Gets the application data path.</summary>
@@ -46,14 +59,14 @@
         ''' A directory path with read and write access for use as the application's data path.
         ''' </returns>
         Private Function GetAppDataPath(Optional autoCreate As Boolean = True)
-            Dim appDir = New System.IO.FileInfo(_exeFile).Directory
+            Dim appDir = (New System.IO.FileInfo(_exeFile)).Directory
             Dim appDrive = New System.IO.DriveInfo(appDir.Root.FullName)
             Dim localAppData = IO.Path.Combine(
-                               IO.Path.Combine(System.Environment.GetEnvironmentVariable("appdata"),
+                               IO.Path.Combine(System.Environment.GetEnvironmentVariable("AppData"),
                                                Me.Info.CompanyName),
                                                Me.Info.ProductName)
             Dim programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)
-            Dim windir = Environment.GetEnvironmentVariable("windir")
+            Dim windir = Environment.GetEnvironmentVariable("winDir")
 
             Dim appDataPath = ""
             Try
@@ -67,7 +80,7 @@
                 If (appDir.FullName.Contains(programFiles) OrElse appDir.FullName.Contains(windir)) Then
                     appDataPath = localAppData
                 Else
-                    appDataPath = appDir.FullName
+                    appDataPath = IO.Path.Combine(appDir.FullName, "AppData")
                 End If
 
             Catch ex As Exception
@@ -81,5 +94,38 @@
 
             Return appDataPath
         End Function
+
+        ''' <summary>Resolves the assembly when an assembly resolution fails.</summary>
+        ''' <param name="sender">The sender of the event.</param>
+        ''' <param name="args">The <see cref="ResolveEventArgs"/> instance containing the event data.</param>
+        ''' <returns>The assembly resolved by the event</returns>
+        Private Function ResolveAssembly(sender As Object, args As ResolveEventArgs) As System.Reflection.Assembly
+            Dim desiredAssembly = New Reflection.AssemblyName(args.Name)
+            Dim assemblies = From file In FileIO.FileSystem.GetFiles(My.Application.Info.DirectoryPath,
+                                                                     FileIO.SearchOption.SearchAllSubDirectories,
+                                                                     "*.dll")
+                             Let Info = Reflection.AssemblyName.GetAssemblyName(file)
+                             Where desiredAssembly.Name = Info.Name
+                             Order By Info.Version
+                             Select New With {.FileName = file, Info}
+
+
+            If assemblies.Any() Then
+                Dim sameVersion = From a In assemblies Where a.Info.Version = desiredAssembly.Version
+                Dim lowerVersion = From a In assemblies Where a.Info.Version <= desiredAssembly.Version
+                Dim higherVersion = From a In assemblies Where a.Info.Version >= desiredAssembly.Version
+
+                ' return the same version, 
+                ' otherwise the lowest version of higher versions of the desired assembly,
+                ' otherwise the highest version of lower versions of the desired assembly
+                If sameVersion.Any() Then Return Reflection.Assembly.Load(sameVersion.First().FileName)
+                If higherVersion.Any() Then Return Reflection.Assembly.Load(higherVersion.Last().FileName)
+                If lowerVersion.Any Then Return Reflection.Assembly.Load(lowerVersion.First().FileName)
+            End If
+
+            ' matching assembly not found. return nothing
+            Return Nothing
+        End Function
+
     End Class
 End Namespace
